0e2034cb262de66d46fcbc37f51bfa59dc5c7be7
Yairyeger <yairyeger@walla.com>
2026-06-23 11:57:13 +0300

    Y

---
+++ app.js
@@ -0,0 +1,14 @@
+const festivals = [
+  {
+    id: "rip",
+    name: "Rock im Park 2026",
+    image: "assets/rock-im-park.png",
+    bands: [","]
+  },
+  {
+    id: "wacken",
+    name: "Wacken Open Air 2026",
+    image: "assets/wacken.png",
+    bands: [","]
+  }
+];
